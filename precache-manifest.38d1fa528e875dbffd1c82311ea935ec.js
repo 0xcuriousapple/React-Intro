da423fd1e5d458433d4a3b8d7c67d773679933ac
unknown <abhivispute33@gmail.com>
2020-05-15 16:48:24 +0530

    Updates

---
+++ precache-manifest.38d1fa528e875dbffd1c82311ea935ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b661a89ee465969210487539a293ec",
+    "url": "/react-my-first-app/index.html"
+  },
+  {
+    "revision": "7bc4866492a46a49b31f",
+    "url": "/react-my-first-app/static/css/main.f3900a30.chunk.css"
+  },
+  {
+    "revision": "00e11e9cfb32d08da717",
+    "url": "/react-my-first-app/static/js/2.de88334e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-my-first-app/static/js/2.de88334e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc4866492a46a49b31f",
+    "url": "/react-my-first-app/static/js/main.b509b240.chunk.js"
+  },
+  {
+    "revision": "3905b06b77da313ffe6a",
+    "url": "/react-my-first-app/static/js/runtime-main.226a0a3b.js"
+  }
+]);
